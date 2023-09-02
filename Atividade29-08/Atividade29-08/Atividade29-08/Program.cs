c82d3d064b61b41f832192433b2771d87d368cdd
sabrina mendonça <109186458+sabrainas@users.noreply.github.com>
2023-09-02 16:25:15 -0300

    Add files via upload

---
+++ Atividade29-08/Atividade29-08/Atividade29-08/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade29_08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcao, aumento;
+            double salario = 0, salarioFinal = 0, imposto = 0;
+
+            do
+            {
+                Console.WriteLine("------------------------");
+                Console.WriteLine("Selecione uma das opções abaixo: ");
+                Console.WriteLine("1. Imposto");
+                Console.WriteLine("2. Novo salário");
+                Console.WriteLine("3. Classificação");
+                Console.WriteLine("4. Finalização");
+                Console.WriteLine("------------------------");
+                opcao = Int32.Parse(Console.ReadLine());
+
+                switch (opcao)
+                {
+                //receber o salario de um funcionário, calcular e mostrar o valor do imposto usando as regras a seguir
+                    case 1:
+                        Console.WriteLine("Informe o salário: ");
+                        salario = Double.Parse(Console.ReadLine());
+
+                        if(salario < 500)
+                        {
+                            Console.WriteLine("Descontará 5% de imposto");
+                            imposto = salario * 0.05;
+                            salarioFinal = salario - imposto;
+                            Console.WriteLine("Novo salário: R$" + salarioFinal);
+                        } else if(salario >= 500 && salario <= 850)
+                        {
+                            Console.WriteLine("Descontará 10% de imposto");
+                            imposto = salario * 0.10;
+                            salarioFinal = salario - imposto;
+                            Console.WriteLine("Novo salário: R$" + salarioFinal);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Descontará 15% de imposto");
+                            imposto = salario * 0.15;
+                            salarioFinal = salario - imposto;
+                            Console.WriteLine("Novo salário: R$" + salarioFinal);
+                        }
+                        break;
+                    
+                    case 2:
+                        Console.WriteLine("Informe o salário: ");
+                        salario = Double.Parse(Console.ReadLine());
+
+                        if(salario > 1500)
+                        {
+                            Console.WriteLine("Terá um aumento de R$25,00");
+                            aumento = 25;
+                            salarioFinal = salario + aumento;
+                            Console.WriteLine("Novo salário: R$" + salarioFinal);
+                        } else if(salario >= 750 && salario <= 1500)
+                        {
+                            Console.WriteLine("Terá um aumento de R$50,00");
+                            aumento = 50;
+                            salarioFinal = salario + aumento;
+                            Console.WriteLine("Novo salário: R$" + salarioFinal);
+                        } else if(salario >= 450 && salario < 750)
+                        {
+                            Console.WriteLine("Terá um aumento de R$75,00");
+                            aumento = 25;
+                            salarioFinal = salario + aumento;
+                            Console.WriteLine("Novo salário: R$" + salarioFinal);
+                        }else if(salario < 450)
+                        {
+                            Console.WriteLine("Terá um aumento de R$100,00");
+                            aumento = 100;
+                            salarioFinal = salario + aumento;
+                            Console.WriteLine("Novo salário: R$" + salarioFinal);
+                        }
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Informe o salário: ");
+                        salario = Double.Parse(Console.ReadLine());
+
+                        if(salario <= 750)
+                        {
+                            Console.WriteLine("Mal remunerado");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Bem remunerado");
+                        }
+                        break;
+
+                    default:
+                        break;
+                }
+               
+            } while (opcao != 4);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Atividade2_29-08/Atividade2_29-08/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade2_29_08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String cpf;
+            int numDependentes;
+            double rendaMensal =0, salarioMinimo = 1320, descDependentes = 0.05, salarioMinNovo = 0, aliquota = 0, salarioImposto =0;
+
+            
+            Console.Write("Digite o seu CPF: ");
+            cpf = Console.ReadLine();
+
+            Console.Write("Digite o número de dependentes: ");
+            numDependentes = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a sua renda mensal: ");
+            rendaMensal = Double.Parse(Console.ReadLine());
+
+            if (numDependentes > 0)
+            {
+
+                double descontoDependentes = numDependentes * (salarioMinimo * descDependentes);
+                salarioMinNovo = rendaMensal - descontoDependentes;
+
+                Console.WriteLine("O salario com desconto de dependentes: " + salarioMinNovo);
+            }
+
+            if (salarioMinNovo <= 2 * salarioMinimo)
+            {
+                aliquota = 0.0;
+                Console.WriteLine("Isento");
+            }
+            else if (salarioMinNovo <= 3 * salarioMinimo)
+            {
+                aliquota = 0.05;
+                Console.WriteLine("Alíquota: 5%");
+            }else if (salarioMinNovo <= 5 * salarioMinimo)
+            {
+                aliquota = 0.10;
+                Console.WriteLine("Alíquota: 10%");
+            }else if (salarioMinNovo <= 7 * salarioMinimo)
+            {
+                aliquota = 0.15;
+                Console.WriteLine("Alíquota: 15%");
+            }else
+            {
+                aliquota = 0.20;
+                Console.WriteLine("Alíquota: 20%");
+            }
+
+            salarioImposto = salarioMinNovo * aliquota;
+
+            Console.WriteLine("CPF: " + cpf);
+            Console.WriteLine("Imposto de renda: " + salarioImposto);
+            Console.WriteLine("-----------------------------------");
+            Console.ReadKey();
+        }
+    }
+}
